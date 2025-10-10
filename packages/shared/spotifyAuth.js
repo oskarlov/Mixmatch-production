20d9179bab5ed6f1cdd6e890f27d9b89e398017a
Viktor Hamberg <vhamb@kth.se>
2025-10-10 11:43:59 +0200

    Spotify Auth v1

---
+++ packages/shared/spotifyAuth.js
@@ -0,0 +1,73 @@
+import { ID, SECRET } from "apiConfig.js";
+
+// Code from https://developer.spotify.com/documentation/web-api/tutorials/code-pkce-flow
+export async function redirectToAuth() {
+    const generateRandomString = (length) => {
+        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const values = crypto.getRandomValues(new Uint8Array(length));
+        return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+    }
+
+    const codeVerifier  = generateRandomString(64);
+
+    const sha256 = async (plain) => {
+        const encoder = new TextEncoder()
+        const data = encoder.encode(plain)
+        return window.crypto.subtle.digest('SHA-256', data)
+    }
+
+    const base64encode = (input) => {
+        return btoa(String.fromCharCode(...new Uint8Array(input)))
+        .replace(/=/g, '')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_');
+    }
+
+    const hashed = await sha256(codeVerifier)
+    const codeChallenge = base64encode(hashed);
+
+    const scope = 'user-read-private user-read-email';
+    const authUrl = new URL("https://accounts.spotify.com/authorize")
+    const redirectUri = "temp" // Lägg till correct redirectURI som ligger i Spotify appen
+
+    // generated in the previous step
+    window.localStorage.setItem('code_verifier', codeVerifier);
+
+    const params =  {
+        response_type: 'code',
+        client_id: ID,
+        scope,
+        code_challenge_method: 'S256',
+        code_challenge: codeChallenge,
+        redirect_uri: redirectUri,
+    }
+
+    authUrl.search = new URLSearchParams(params).toString();
+    window.location.href = authUrl.toString();
+}
+
+export function requestToken() {
+    const urlParams = new URLSearchParams(window.location.search);
+    let code = urlParams.get('code');
+    const codeVerifier = localStorage.getItem('code_verifier');
+
+    const url = "https://accounts.spotify.com/api/token";
+    const payload = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+            client_id: clientId,
+            grant_type: 'authorization_code',
+            code,
+            redirect_uri: redirectUri,
+            code_verifier: codeVerifier,
+        }),
+    }
+
+    const body = await fetch(url, payload);
+    const response = await body.json();
+
+    localStorage.setItem('access_token', response.access_token);
+}
