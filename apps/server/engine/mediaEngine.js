0f51a50a4be327015267d8b6334829cea65b0b21
Viktor Hamberg <vhamb@kth.se>
2025-10-19 15:27:11 +0200

    makeTrackList completed

--- apps/server/engine/mediaEngine.js
+++ apps/server/engine/mediaEngine.js
@@ -2,10 +2,19 @@ import { getPlaylistData, getAccessToken } from "./spotifyAuth.js";
 
 
 export async function makeTrackList(playlistID, n) {
-    // Updatera för all info som behövs och utan "id: 'tx'"
+    // Shuffle to get n amount of random tracks from the list
+    function shuffle(a) {
+        for (let i = a.length - 1; i > 0; i--) {
+          const j = (Math.random() * (i + 1)) | 0;
+          [a[i], a[j]] = [a[j], a[i]];
+        }
+        return a;
+    }
+
+    // Saves the correct track info
     function saveCorrectInfoCB(trackObject, i) {
         return {
-            id:`t${i+1}`,
+            id: trackObject.id,// `t${i+1}`,
             title: trackObject.name,
             artist: trackObject.artists[0].name
         };
@@ -32,7 +41,7 @@ export async function makeTrackList(playlistID, n) {
     const dataPL = await getPlaylistData(token, playlistID);
     console.log(dataPL);
     // lägg till randomize innan slice
-    const tracks = dataPL.slice(0, n).map(saveCorrectInfoCB);
+    const tracks = shuffle(dataPL).slice(0, n).map(saveCorrectInfoCB);
     console.log(tracks);
     return tracks;
 }
